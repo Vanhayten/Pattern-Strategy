17d7aeb722223dea2202875891f44b8c120e3754
ayoub chaib <watchpro2@gmail.com>
2026-06-06 16:11:27 +0100

    Part 1: Basic Structure with Strategy, Context, and Default Strategy

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Pattern-Strategy
+
+## Part 1
+- Basic interface `Strategy`
+- Basic `Context` implementation with `DefaultStrategyImpl`
+- Static execution in `Main`
+
---
+++ src/main/java/org/example/DefaultStrategyImpl.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class DefaultStrategyImpl implements Strategy {
+    @Override
+    public void operationStrategy() {
+        System.out.println("===================================");
+        System.out.println("=========== Default Strategy ============");
+        System.out.println(":::::::::::::::::::::::::::::::::::");
+    }
+}
